e450413917546bdae6b193ccb2a7a79634c96830
jjucaa <joaovitorsilveriocoelho@yahoo.com.br>
2021-04-24 16:09:25 -0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 const express = require('express')
-const route = require('./routes/charRoute')
+const swaggerUi = require('swagger-ui-express')
+const route = require('./routes/char.Route')
+const swggerDocument = require('./documentation.json')
 
 const app = express()
 
+const port = process.env.PORT || 8080
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swggerDocument))
+
 app.use(express.json())
 app.use('/' , route)
-
-const port = process.env.PORT || 8080
 
 app.listen(port, () => {
     console.log(`A porta ${port} é minha agora`)
